0d4c624273b605322f4e55c32a8b782483b0162b
Novami <mfahmia1125@gmail.com>
2026-01-08 13:37:18 +0700

    update 1.1

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -10,7 +10,7 @@ export const AboutSection = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                 <div className="space-y-6">
-                    <h3 className="text-2xl font-semibold">Passionate To You</h3>
+                    <h3 className="text-2xl font-semibold">Passionate To Electrical and Robotics</h3>
 
                     <p className="text-muted-foreground">
                         Saya lahir pada hari sabtu tanggal 25 November 2006 di Bandung, olahraga favorit Saya adalah lari dan bersepeda,
